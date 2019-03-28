14ebd8bf526e24a81bf2a4708a1e37554e934fd0
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-28 14:30:54 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,6 +22,7 @@ def bonus
 
   # Change Romeo and Juilet status to "dead"
   epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
